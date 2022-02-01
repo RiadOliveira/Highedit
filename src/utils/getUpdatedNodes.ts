d2640fea963f98980660d4dcf5d38166eb5a50ef
RiadOliveira <riad.oliveira@hotmail.com>
2022-01-31 23:34:37 -0300

    Multitag all text formatting

--- src/utils/getUpdatedNodes.ts
+++ src/utils/getUpdatedNodes.ts
@@ -9,46 +9,70 @@ const getUpdatedNodes = (
 ): (Node | string)[] => {
   const selection = window.getSelection() as Selection;
   const textRef = inputRef.current as HTMLPreElement;
-  const { startContainer, endContainer } = selection.getRangeAt(0);
-
-  const startNode =
-    startContainer.parentNode !== textRef
-      ? startContainer.parentNode
-      : startContainer;
-
-  const endNode =
-    endContainer.parentNode !== textRef
-      ? endContainer.parentNode
-      : endContainer;
+  const range = selection.getRangeAt(0);
 
   const childrenArray = Array.from(textRef.childNodes);
-  const firstIndexSelected = childrenArray.indexOf(startNode as ChildNode);
-  const lastIndexSelected = childrenArray.indexOf(endNode as ChildNode);
+  const clonedNodes = range.cloneContents().childNodes;
 
-  const { anchorOffset: start, focusOffset: end, anchorNode } = selection;
+  const { anchorOffset: start, focusOffset: end } = selection;
   const points = {
     start: Math.min(start, end),
     end: Math.max(start, end),
   };
-  const parentNode = anchorNode?.parentNode;
 
   // Iterate through all children of the created text.
   const inputNodes: (Node | string)[] = childrenArray.map((child, index) => {
-    if (index < firstIndexSelected || index > lastIndexSelected) return child;
+    let selectedText = '';
+    let isChild = false;
+    let comparativeNode: Node | null | undefined;
 
-    const isChild = Array.from(child.childNodes).includes(
-      startContainer as ChildNode,
-    );
+    if (clonedNodes.length === 1) {
+      const { anchorNode } = selection;
+      const { parentNode } = anchorNode as Node;
+
+      comparativeNode = parentNode !== textRef ? parentNode : anchorNode;
+
+      isChild = Array.from(child.childNodes).includes(
+        comparativeNode as ChildNode,
+      );
+
+      // If the child isn't the selected, return it without changes.
+      if (comparativeNode !== child && !isChild) return child;
+
+      selectedText = selection.toString();
+    } else {
+      const { startContainer, endContainer } = range;
+
+      const getExtremeIndex = (point: 'begin' | 'end') => {
+        const container = point === 'begin' ? startContainer : endContainer;
+
+        return childrenArray.findIndex(
+          searchChild =>
+            searchChild === container ||
+            searchChild === (container.parentNode as Node),
+        );
+      };
+
+      const startNodeIndex = getExtremeIndex('begin');
+      const endNodeIndex = getExtremeIndex('end');
+
+      if (index < startNodeIndex || index > endNodeIndex) return child;
+
+      comparativeNode = clonedNodes.item(index);
+
+      const { textContent } = comparativeNode;
+      selectedText = textContent || '';
+    }
 
     switch (property.type) {
       case 'tag':
-        return cases.tag(points, selection.toString(), property.name, child);
+        return cases.tag(points, selectedText, property.name, child);
 
       case 'special':
         return specialFunctions.link(
           child,
-          startNode as Node,
-          selection.toString(),
+          comparativeNode as Node,
+          selectedText,
         );
 
       default: {
@@ -57,14 +81,14 @@ const getUpdatedNodes = (
           style: { hasTag, justText },
         } = cases;
 
-        if (parentNode === textRef) return justText(points, code, child);
+        if (child.nodeName === '#text') return justText(points, code, child);
 
         const element = child.firstChild?.parentElement;
 
         if (element) {
           return isChild
-            ? hasTag.isChild(element, startNode as Node, code)
-            : hasTag.notChild(element, selection.toString(), points, code);
+            ? hasTag.isChild(element, comparativeNode as Node, code)
+            : hasTag.notChild(element, selectedText, points, code);
         }
       }
     }
